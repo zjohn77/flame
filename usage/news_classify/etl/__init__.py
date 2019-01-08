3d9b0f96e37cb4878f315a9cda62b967460c8794
jj <tojohnjung@gmail.com>
2019-01-07 23:41:23 -0500

    relinked paths after refactor

--- usage/news_classify/etl/__init__.py
+++ usage/news_classify/etl/__init__.py
@@ -1,6 +1,2 @@
 from .extract_bbc import extract_data
 from .transform_bbc import reshape
-
-def bbc_data_pipeline():
-   return reshape(extract_data('data/bbc')
-                 )
--- usage/news_classify/news_classify.py
+++ usage/news_classify/news_classify.py
@@ -1,5 +1,15 @@
-from build_model import build_model
-from etl import bbc_data_pipeline
+import sys
+from pathlib import Path
+module_path = Path(__file__).resolve().parents[2] ## cd ../..
+sys.path.insert(0, str(module_path))
 
-data, target = bbc_data_pipeline()
-model = build_model()
+from etl import extract_data, reshape
+from flame import main
+
+def bbc_data_pipeline():
+   return reshape(extract_data('data/')
+                 )
+
+if __name__ == '__main__':
+   data, target = bbc_data_pipeline()
+   model = main(data, target)
